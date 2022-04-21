11a30e137c3b35cdd273fef32a04768c7e773063
Matthias Kettner <matthias.kettner@student.tuwien.ac.at>
2022-04-21 11:20:12 +0200

    - First plots in exampleAnalysis.py - Exercise from introduction to ROOT

--- plots/plotsMatthias/exampleAnalysis.py
+++ plots/plotsMatthias/exampleAnalysis.py
@@ -40,7 +40,7 @@ import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument('--logLevel',       action='store',      default='INFO', nargs='?', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'], help="Log level for logging")
 argParser.add_argument('--plot_directory', action='store', default='MTopCorrelations_v1')
-argParser.add_argument('--selection',      action='store', default='nAK82p')
+argParser.add_argument('--selection',      action='store', default='nAK81p')
 argParser.add_argument('--era',            action='store', type=str, default="UL2018")
 args = argParser.parse_args()
 
@@ -59,7 +59,7 @@ mc = [UL2018.TTbar]
 lumi_scale = 60
 
 ################################################################################
-# Correlator Hist 
+# Correlator Hist
 # hist = ROOT.TH1F("Correlator", "dR", 10, 0, 3)
 
 ################################################################################
@@ -105,19 +105,90 @@ def drawPlots(plots):
 sequence       = []
 
 
+def gen_tops(event, sample):
+    top_vec = ROOT.TLorentzVector()
+    anti_top_vec = ROOT.TLorentzVector()
+    for i in range(event.nGenPart):
+        if event.GenPart_pdgId[i] == 6:
+            top_vec.SetPtEtaPhiM(event.GenPart_pt[i], event.GenPart_eta[i], event.GenPart_phi[i], event.GenPart_m[i])
+        elif event.GenPart_pdgId[i] == -6:
+            anti_top_vec.SetPtEtaPhiM(event.GenPart_pt[i], event.GenPart_eta[i], event.GenPart_phi[i], event.GenPart_m[i])
+
+    top_lep = ROOT.TLorentzVector()
+    top_had = ROOT.TLorentzVector()
+    for i in range(event.nGenPart):
+         if abs(event.GenPart_pdgId[i]) in [11, 13, 15]:
+            if event.GenPart_grmompdgId[i] == 6:
+                top_lep = top_vec
+                top_had = anti_top_vec
+            elif event.GenPart_grmompdgId[i] == -6:
+                top_lep = anti_top_vec
+                top_had = top_vec
+
+    # event.top_had_pt = top_had.Pt()
+
+    quark_vec = ROOT.TLorentzVector()
+    anti_quark_vec = ROOT.TLorentzVector()
+    bottom_vec = ROOT.TLorentzVector()
+    for i in range(event.nGenPart):
+        if event.GenPart_pdgId[i] in range(1, 7) and abs(event.GenPart_grmompdgId[i]) == 6:
+            quark_vec.SetPtEtaPhiM(event.GenPart_pt[i], event.GenPart_eta[i], event.GenPart_phi[i], event.GenPart_m[i])
+        elif event.GenPart_pdgId[i] in range(-1, -7, -1) and abs(event.GenPart_grmompdgId[i]) == 6:
+            anti_quark_vec.SetPtEtaPhiM(event.GenPart_pt[i], event.GenPart_eta[i], event.GenPart_phi[i], event.GenPart_m[i])
+        elif event.GenPart_pdgId[i] == 5 and abs(event.GenPart_mompdgId[i]) == 6:
+            bottom_vec.SetPtEtaPhiM(event.GenPart_pt[i], event.GenPart_eta[i], event.GenPart_phi[i], event.GenPart_m[i])
+
+    jets = []
+    for i in range(event.nGenJetAK8):
+        jets.append(ROOT.TLorentzVector())
+        jets[-1].SetPtEtaPhiM(event.GenJetAK8_pt[i], event.GenJetAK8_eta[i], event.GenJetAK8_phi[i], event.GenJetAK8_mass[i])
+    deltas_t = [jets[j].DeltaR(top_had) for j in range(event.nGenJetAK8)]
+    event.delta_min = min(deltas_t)
+    nearest_jet_idx_t = np.argmin(deltas_t)
+    event.nearest_jet_mass_t = jets[nearest_jet_idx_t].M()
+    event.nearest_jet_pt = jets[nearest_jet_idx_t].Pt()
+    event.nearest_jet_eta = jets[nearest_jet_idx_t].Eta()
+    event.nearest_jet_phi = jets[nearest_jet_idx_t].Phi()
+
+    delta_q = jets[nearest_jet_idx_t].DeltaR(quark_vec)
+    delta_aq = jets[nearest_jet_idx_t].DeltaR(anti_quark_vec)
+    delta_b = jets[nearest_jet_idx_t].DeltaR(bottom_vec)
+
+    event.merged_top = False
+    event.merged_q_aq_b = False
+
+    if event.delta_min < 0.8:
+        event.merged_top = True
+
+        if delta_q < 0.8 and delta_aq < 0.8 and delta_b < 0.8:
+            event.merged_q_aq_b = True
+
+    event.matched_jet_nCons = 0
+    event.matched_jet_Cons_pt = []
+    for k in range(event.nGenJetAK8_cons):
+        if event.GenJetAK8_cons_jetIndex[k] == nearest_jet_idx_t:
+            event.matched_jet_nCons += 1
+            event.matched_jet_Cons_pt.append(event.GenJetAK8_cons_pt[k])
+
+    event.matched_jet_Cons_pt_ratio = [elem/event.nearest_jet_pt for elem in event.matched_jet_Cons_pt]
+
+
+sequence.append(gen_tops)
+
 ################################################################################
 # Read variables
 
 read_variables = [
     "nGenPart/I",
-    "GenPart[pt/F,eta/F,phi/F,m/F,pdgId/I,mompdgId/i]",
-    # "nGenJetAK8/I",
-    # "GenJetAK8[pt/F,eta/F,phi/F,mass/F]",
+    "GenPart[pt/F,eta/F,phi/F,m/F,pdgId/I,mompdgId/I,grmompdgId/I]",
+    "nGenJetAK8/I",
+    "GenJetAK8[pt/F,eta/F,phi/F,mass/F]",
     "nPFJetAK8/I",
     "PFJetAK8[pt/F,eta/F,phi/F,mass/F]",
     "nPFJetAK8_cons/I",
     VectorTreeVariable.fromString( "PFJetAK8_cons[pt/F,eta/F,phi/F,mass/F,pdgId/I,jetIndex/I]", nMax=1000),
-    
+    "nGenJetAK8_cons/I",
+    VectorTreeVariable.fromString( "GenJetAK8_cons[pt/F,eta/F,phi/F,mass/F,pdgId/I,jetIndex/I]", nMax=1000),
 ]
 
 ################################################################################
@@ -126,12 +197,12 @@ read_variables = [
 # weight_ = lambda event, sample: 1. if sample.isData else lumi_scale/1000.
 weight_ = lambda event, sample: 1.
 
-for sample in mc: 
+for sample in mc:
     sample.style = styles.fillStyle(sample.color)
 
 for sample in mc:
     sample.weight = lambda event, sample: 1.
-    
+
 stack = Stack(mc)
 
 # Use some defaults
@@ -150,24 +221,143 @@ plots.append(Plot(
 ))
 
 plots.append(Plot(
-    name = "mjet",
-    texX = 'm_{jet} [GeV]', texY = 'Number of Events',
-    attribute = lambda event, sample: event.PFJetAK8_mass[0],
-    binning=[25, 0., 500.],
-))
-
-plots.append(Plot(
     name = "ptjet",
     texX = 'Leading AK8 jet p_{T} [GeV]', texY = 'Number of Events',
     attribute = lambda event, sample: event.PFJetAK8_pt[0],
     binning=[25, 0., 500.],
 ))
 
+plots.append(Plot(
+    name = "delta_min",
+    texX = 'Delta between nearest jet and hadronic top [rad]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.delta_min,
+    binning=[25, 0., 7.],
+))
+
+plots.append(Plot(
+    name = "jet_mass",
+    texX = 'Mass of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_mass_t,
+    binning=[25, 0., 500.],
+))
+
+plots.append(Plot(
+    name = "jet_pt",
+    texX = 'P_{t} of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_pt,
+    binning=[25, 0., 500.],
+))
+
+plots.append(Plot(
+    name = "jet_eta",
+    texX = '#eta of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_eta,
+    binning=[25, 3.5, 3.5],
+))
+
+plots.append(Plot(
+    name = "jet_phi",
+    texX = '#phi of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_phi,
+    binning=[25, 3.5, 3.5],
+))
+
+plots.append(Plot(
+    name = "cut_delta_min",
+    texX = 'Delta between nearest jet and hadronic top [rad]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.delta_min if event.merged_top else float('nan'),
+    binning=[25, 0., 7.],
+))
+
+plots.append(Plot(
+    name = "cut_jet_mass",
+    texX = 'Mass of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_mass_t if event.merged_top else float('nan'),
+    binning=[25, 0., 500.],
+))
+
+plots.append(Plot(
+    name = "cut_jet_pt",
+    texX = 'P_{t} of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_pt if event.merged_top else float('nan'),
+    binning=[25, 0., 500.],
+))
+
+plots.append(Plot(
+    name = "cut_jet_eta",
+    texX = '#eta of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_eta if event.merged_top else float('nan'),
+    binning=[25, 3.5, 3.5],
+))
+
+plots.append(Plot(
+    name = "cut_jet_phi",
+    texX = '#phi of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_phi if event.merged_top else float('nan'),
+    binning=[25, 3.5, 3.5],
+))
+
+plots.append(Plot(
+    name = "cut_delta_min_2",
+    texX = 'Delta between nearest jet and hadronic top [rad]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.delta_min if event.merged_q_aq_b else float('nan'),
+    binning=[25, 0., 7.],
+))
+
+plots.append(Plot(
+    name = "cut_jet_mass_2",
+    texX = 'Mass of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_mass_t if event.merged_q_aq_b else float('nan'),
+    binning=[25, 0., 500.],
+))
+
+plots.append(Plot(
+    name = "cut_jet_pt_2",
+    texX = 'P_{t} of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_pt if event.merged_q_aq_b else float('nan'),
+    binning=[25, 0., 500.],
+))
+
+plots.append(Plot(
+    name = "cut_jet_eta_2",
+    texX = '#eta of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_eta if event.merged_q_aq_b else float('nan'),
+    binning=[25, 3.5, 3.5],
+))
+
+plots.append(Plot(
+    name = "cut_jet_phi_2",
+    texX = '#phi of jet next to hadronic top [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.nearest_jet_phi if event.merged_q_aq_b else float('nan'),
+    binning=[25, 3.5, 3.5],
+))
+
+plots.append(Plot(
+    name = "number_of_cons",
+    texX = 'Number of constituents in matched jet', texY = 'Number of Events',
+    attribute = lambda event, sample: event.matched_jet_nCons,
+    binning=[25, 0., 150.],
+))
+
+plots.append(Plot(
+    name = "cons_pt",
+    texX = 'P_{t} of constituents of matched jet [GeV]', texY = 'Number of Events',
+    attribute = lambda event, sample: event.matched_jet_Cons_pt,
+    binning=[25, 0., 50.],
+))
+
+plots.append(Plot(
+    name = "cons_pt_ratio",
+    texX = 'P_{t} ratio of constituents of matched jet', texY = 'Number of Events',
+    attribute = lambda event, sample: event.matched_jet_Cons_pt_ratio,
+    binning=[25, 0., 0.25],
+))
+
 
 plotting.fill(plots, read_variables = read_variables, sequence = sequence)
 
 drawPlots(plots)
-    
-# print hist.Integral()    
-    
+
+# print hist.Integral()
+
 logger.info( "Done with prefix %s and selectionString %s", args.selection, cutInterpreter.cutString(args.selection) )
---
+++ plots/plotsMatthias/root_exercise_hist.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from ROOT import TH1F
+
+hist = TH1F('exercise_hist', 'Exercise Histogram', 10, 0, 100)
+hist.Fill(11.3)
+hist.Fill(25.4)
+hist.Fill(18.1)
+for number in np.square(np.arange(10)):
+    hist.Fill(number)
+print('Histrogram was created and filled.')
+hist.Draw()
+
+hist_mean = hist.GetMean()
+hist_rms = hist.GetRMS()
+
+print('Mean value of the histogram: {:.2f}'.format(hist_mean))
+print('RMS value of the histogram: {:.2f}'.format(hist_rms))
+
+hist_integral = hist.Integral()
+max_bin = hist.GetMaximumBin()
+max_bin_content = hist.GetBinContent(max_bin)
+
+hist.GetYaxis().SetTitle('entries')
+hist.SetLineColor(5)
+
+hist.Print('exercise_hist.png')
