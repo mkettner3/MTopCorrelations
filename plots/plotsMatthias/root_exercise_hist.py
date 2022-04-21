9dec6fb5545b65e0df340f0f2cd6e1937380a706
Matthias Kettner <matthias.kettner@student.tuwien.ac.at>
2022-04-21 12:57:05 +0200

    Improved ROOT diagram drawing using Canvas and saving to ROOT file

--- plots/plotsMatthias/root_exercise_hist.py
+++ plots/plotsMatthias/root_exercise_hist.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from ROOT import TH1F
+from ROOT import TH1F, TCanvas, TFile, kRed
 
 hist = TH1F('exercise_hist', 'Exercise Histogram', 10, 0, 100)
 hist.Fill(11.3)
@@ -10,7 +10,6 @@ hist.Fill(18.1)
 for number in np.square(np.arange(10)):
     hist.Fill(number)
 print('Histrogram was created and filled.')
-hist.Draw()
 
 hist_mean = hist.GetMean()
 hist_rms = hist.GetRMS()
@@ -23,6 +22,13 @@ max_bin = hist.GetMaximumBin()
 max_bin_content = hist.GetBinContent(max_bin)
 
 hist.GetYaxis().SetTitle('entries')
-hist.SetLineColor(5)
+hist.SetLineColor(kRed)
 
-hist.Print('exercise_hist.png')
+c = TCanvas("canvas", "canvas", 800, 600)
+hist.Draw()
+c.Print('exercise_hist.png')
+
+f = TFile('tests.root', 'RECREATE')
+f.cd()
+hist.Write('funny_histogram')
+f.Close()
