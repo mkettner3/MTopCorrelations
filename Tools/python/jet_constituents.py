1ba51fa7d6869e0ed67560958b3dacdcf8b607d6
Matthias Kettner <matthias.kettner@student.tuwien.ac.at>
2022-06-19 20:05:22 +0200

    Removed unnecessary class definition; Made calculation of Top data or W data optional

--- Tools/python/jet_constituents.py
+++ Tools/python/jet_constituents.py
@@ -8,34 +8,31 @@ from ROOT import TLorentzVector
 from typing import Any
 
 
-class JetConstituents(object):
+def get_jet_constituents(event, index, max_numb_of_cons=None):
+    # type: (Any, int, int) -> list
+    """
+    Method to obtain the lorentz-vectors of the constituents of a jet.
 
-    @staticmethod
-    def get(event, index, max_numb_of_cons=None):
-        # type: (Any, int, int) -> list
-        """
-        Method to obtain the lorentz-vectors of the constituents of a jet.
+    :param event: A ROOT event.
+    :param index: int; The index of the respective jet.
+    :param max_numb_of_cons: int; The maximum number of jet constituents, which should be returned.
 
-        :param event: A ROOT event.
-        :param index: int; The index of the respective jet.
-        :param max_numb_of_cons: int; The maximum number of jet constituents, which should be returned.
+    :return: list; A list of lorentz-vectors of the constituents of the jet.
+    """
+    constituents = []
+    for i in range(event.nGenJetAK8_cons):
+        if event.GenJetAK8_cons_jetIndex[i] == index:
+            particle = TLorentzVector()
+            particle.SetPtEtaPhiM(event.GenJetAK8_cons_pt[i], event.GenJetAK8_cons_eta[i],
+                                  event.GenJetAK8_cons_phi[i], event.GenJetAK8_cons_mass[i])
+            constituents.append(particle)
 
-        :return: list; A list of lorentz-vectors of the constituents of the jet.
-        """
-        constituents = []
-        for i in range(event.nGenJetAK8_cons):
-            if event.GenJetAK8_cons_jetIndex[i] == index:
-                particle = TLorentzVector()
-                particle.SetPtEtaPhiM(event.GenJetAK8_cons_pt[i], event.GenJetAK8_cons_eta[i],
-                                      event.GenJetAK8_cons_phi[i], event.GenJetAK8_cons_mass[i])
-                constituents.append(particle)
+    def get_pt(part):
+        return part.Pt()
 
-        def get_pt(part):
-            return part.Pt()
+    constituents.sort(key=get_pt, reverse=True)
+    if isinstance(max_numb_of_cons, int):
+        if len(constituents) > max_numb_of_cons:
+            constituents = constituents[:max_numb_of_cons]
 
-        constituents.sort(key=get_pt, reverse=True)
-        if isinstance(max_numb_of_cons, int):
-            if len(constituents) > max_numb_of_cons:
-                constituents = constituents[:max_numb_of_cons]
-
-        return constituents
+    return constituents
--- Tools/python/triplet_maker.py
+++ Tools/python/triplet_maker.py
@@ -7,44 +7,52 @@ Function to produce triplets.
 import numpy as np
 
 
-class Triplets(object):
+def make_triplets(jet_pt, particle_vectors, n=2, top_data=True, w_data=True, pt_value=True):
+    # type: (float, list, int, bool, bool, bool) -> np.ndarray
+    """
+    Method to produce triplets from the particle vectors.
 
-    @staticmethod
-    def make(jet_pt, particle_vectors, n=2):
-        # type: (float, list, int) -> np.ndarray
-        """
-        Method to produce triplets from the particle vectors.
+    :param jet_pt: float; The Pt of the jet from the top quark.
+    :param particle_vectors: list of ROOT.TLorentzVector(); The lorentz-vectors of the particles inside the
+        mentioned jet of one event.
+    :param n: integer; Order of energy weighting. Default is quadratic order (n=2).
+    :param top_data: boolean; Decides if the maximum of delta_zeta is returned within the numpy array.
+    :param w_data: boolean; Decides if the delta_zeta between legs and shortest side are returned within the
+        numpy array.
+    :param pt_value: boolean; Decides if the jet_pt value is returned within the numpy array.
 
-        :param jet_pt: float; The Pt of the jet from the top quark.
-        :param particle_vectors: list of ROOT.TLorentzVector(); The lorentz-vectors of the particles inside the
-            mentioned jet of one event.
-        :param n: integer; Order of energy weighting. Default is quadratic order (n=2).
+    :return: two-dimensional numpy array; Rows are triplets and Columns are [3*Zeta, Weight (w), maximum of
+        delta_zeta, the delta_zeta between the legs of an isosceles triangle, the length of the shortest triangle
+        side, the pt of the surrounding jet]
+    """
+    w = []
+    zeta_value = []
 
-        :return: two-dimensional numpy array; Rows are triplets and Columns are [3*Zeta, Weight (w), maximum of
-            delta_zeta, the delta_zeta between the legs of an isosceles triangle, the length of the shortest triangle
-            side, the pt of the surrounding jet]
-        """
-        w = []
-        zeta_value = []
+    for i in range(len(particle_vectors)):
+        for j in range(i+1, len(particle_vectors)):
+            for k in range(j+1, len(particle_vectors)):
+                zeta_value.append([particle_vectors[i].DeltaR(particle_vectors[j]),
+                                   particle_vectors[i].DeltaR(particle_vectors[k]),
+                                   particle_vectors[j].DeltaR(particle_vectors[k])])
 
-        for i in range(len(particle_vectors)):
-            for j in range(i+1, len(particle_vectors)):
-                for k in range(j+1, len(particle_vectors)):
-                    zeta_value.append([particle_vectors[i].DeltaR(particle_vectors[j]),
-                                       particle_vectors[i].DeltaR(particle_vectors[k]),
-                                       particle_vectors[j].DeltaR(particle_vectors[k])])
+                w.append((particle_vectors[i].Pt() * particle_vectors[j].Pt() * particle_vectors[k].Pt())**n / ((jet_pt**3)**n))
 
-                    w.append((particle_vectors[i].Pt() * particle_vectors[j].Pt() * particle_vectors[k].Pt())**n / ((jet_pt**3)**n))
+    zeta_value = np.array(zeta_value, dtype=np.float32)
+    zeta_value.sort(axis=1)
+    three_zeta = zeta_value.sum(axis=1)
 
-        zeta_value = np.array(zeta_value, dtype=np.float32)
-        zeta_value.sort(axis=1)
-        three_zeta = zeta_value.sum(axis=1)
+    triplet_param_list = [three_zeta, np.array(w, dtype=np.float32)]
 
-        max_delta_zeta = zeta_value[:, 0] - zeta_value[:, 2]
-        delta_legs = zeta_value[:, 0] - zeta_value[:, 1]
-        shortest_side = zeta_value[:, 2]
+    if top_data:
+        triplet_param_list.append(zeta_value[:, 0] - zeta_value[:, 2])          # max_delta_zeta
 
-        triplet_params = np.stack([three_zeta, np.array(w, dtype=np.float32), max_delta_zeta,
-                                   delta_legs, shortest_side, np.full((len(w)), jet_pt, dtype=np.float32)], axis=1)
+    if w_data:
+        triplet_param_list.append(zeta_value[:, 0] - zeta_value[:, 1])          # delta_legs
+        triplet_param_list.append(zeta_value[:, 2])                             # shortest_side
 
-        return triplet_params
+    if pt_value:
+        triplet_param_list.append(np.full((len(w)), jet_pt, dtype=np.float32))
+
+    triplet_params = np.stack(triplet_param_list, axis=1)
+
+    return triplet_params
