475f71b6d3bf0042f3273c63044e03dd4d29480f
Matthias Kettner <matthias.kettner@student.tuwien.ac.at>
2022-06-19 20:01:50 +0200

    Write computing time in HDF5 file

--- plots/plotsMatthias/calc_triplet_data.py
+++ plots/plotsMatthias/calc_triplet_data.py
@@ -7,8 +7,8 @@ Script to produce numpy arrays of triplet parameters.
 import numpy as np
 import time
 from MTopCorrelations.samples.nanoTuples_UL_RunII_nanoAOD import UL2018
-from MTopCorrelations.Tools.triplet_maker import Triplets
-from MTopCorrelations.Tools.jet_constituents import JetConstituents
+from MTopCorrelations.Tools.triplet_maker import make_triplets
+from MTopCorrelations.Tools.jet_constituents import get_jet_constituents
 from ROOT import TLorentzVector, TNtuple, TFile
 from RootTools.core.TreeVariable import VectorTreeVariable
 import argparse
@@ -61,17 +61,16 @@ def calc_triplet_data(sample):
     r.start()
     while r.run():                                                              # Event-Loop
         nearest_jet_idx, nearest_jet_pt = find_hadronic_jet(r.event)
-
-        jet_constituents = JetConstituents.get(event=r.event, index=nearest_jet_idx, max_numb_of_cons=50)
+        jet_constituents = get_jet_constituents(event=r.event, index=nearest_jet_idx, max_numb_of_cons=50)
 
         if len(jet_constituents) > 0:
-            triplets.append(Triplets.make(jet_pt=nearest_jet_pt, particle_vectors=jet_constituents))
+            triplets.append(make_triplets(jet_pt=nearest_jet_pt, particle_vectors=jet_constituents))
 
     return np.concatenate(triplets, axis=0)
 
 
-def save_triplets_to_hdf5_file(triplets_data, filename):
-    # type: (np.ndarray, str) -> None
+def save_triplets_to_hdf5_file(triplets_data, filename, calc_time):
+    # type: (np.ndarray, str, float) -> None
 
     import h5py
 
@@ -79,6 +78,7 @@ def save_triplets_to_hdf5_file(triplets_data, filename):
         ds = f.create_dataset(name='triplet-parameters', dtype='float32', data=triplets_data)
         description = 'Rows are triplets and Columns are [3*Zeta, Weight (w), maximum of delta_zeta, the delta_zeta between the legs of an isosceles triangle, the length of the shortest triangle side, the pt of the surrounding jet]'
         ds.attrs.create(name='description', data=description)
+        ds.attrs.create(name='computing time', data=calc_time, dtype='float32')
 
 
 def save_triplets_to_root_file(triplets_data, filename):
@@ -96,7 +96,6 @@ def save_triplets_to_root_file(triplets_data, filename):
 
 
 if __name__ == '__main__':
-    start = time.time()
     argParser = argparse.ArgumentParser(description='Argument parser')    # So #SPLIT100 can be used in the bash script.
     argParser.add_argument('--nJobs', action='store', nargs='?', type=int, default=1)
     argParser.add_argument('--job', action='store', type=int, default=0)
@@ -106,9 +105,11 @@ if __name__ == '__main__':
     samples = [sample.split(n=args.nJobs, nSub=args.job) for sample in mc_]
 
     for sample in samples:
+        start = time.time()
         all_triplets = calc_triplet_data(sample=sample)
+        end = time.time()
         save_triplets_to_hdf5_file(triplets_data=all_triplets,
-                                   filename='triplet_files/EWC_triplets_{:}_{:02}.h5'.format(sample.name[:11], args.job))
+                                   filename='triplet_files/EWC_triplets_{:}_{:02}.h5'.format(sample.name[:11], args.job),
+                                   calc_time=end-start)
 
-    end = time.time()
-    print('Executing calc_triplet_data.py took {:.0f}:{:.2f} minutes:seconds.'.format((end-start)//60, (end-start)%60))
+    # print('Executing calc_triplet_data.py took {:.0f}:{:.2f} min:sec.'.format((end-start)//60, (end-start)%60))
