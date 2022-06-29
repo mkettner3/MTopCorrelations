Implemented make_triplets_and_cut() to apply the Top- and W-Cut before calculating the triplet values; Bugfix for multi-sample-calculation: np_hist list-generation has falsely pointed to the same list

plots/plotsMatthias/calc_triplet_and_hist.py
@@ -7,10 +7,11 @@ Script to calculate triplets and generate histograms event-wise. The triplet dat
 import numpy as np
 import time
 from MTopCorrelations.samples.nanoTuples_UL_RunII_nanoAOD import UL2018
-from MTopCorrelations.Tools.triplet_maker import make_triplets
+from MTopCorrelations.Tools.triplet_maker import make_triplets_and_cut
 from MTopCorrelations.Tools.jet_constituents import get_jet_constituents
 from calc_triplet_data import find_hadronic_jet
 from gen_hist_from_triplets import store_np_hist_in_root
+import ROOT
 from RootTools.core.TreeVariable import VectorTreeVariable
 import argparse
 
@@ -25,7 +26,8 @@ def calc_triplets_and_hist(samples, pt_jet_ranges, max_delta_zeta=None, delta_le
         "nGenJetAK8_cons/I",
         VectorTreeVariable.fromString("GenJetAK8_cons[pt/F,eta/F,phi/F,mass/F,pdgId/I,jetIndex/I]", nMax=1000)]
 
-    np_hists = [[np.full(nbins, 0, dtype=np.float32)]*len(pt_jet_ranges)]*len(samples)
+    np_hists = [[np.full(nbins, 0, dtype=np.float32) for _ in range(len(pt_jet_ranges))] for _ in range(len(samples))]
+    # hists = [[ROOT.TH1F("Correlator", "3 #zeta", nbins, hist_range[0], hist_range[1]) for _ in range(len(pt_jet_ranges))] for _ in range(len(samples))]
 
     for h, sample in enumerate(samples):
         r = sample.treeReader(variables=read_variables, selectionString="Sum$(GenJetAK8_pt>400)>=1")
@@ -33,7 +35,6 @@ def calc_triplets_and_hist(samples, pt_jet_ranges, max_delta_zeta=None, delta_le
         global count
         r.start()
         while r.run():                                                              # Event-Loop
-            # count += 1
             # if count % 100 == 0:
             #     print('Event {} is calculated.'.format(count))
             hadronic_jet_idx, hadronic_jet_pt = find_hadronic_jet(r.event, merge_tolerance=0.8, jet_pt_min=400)
@@ -42,43 +43,46 @@ def calc_triplets_and_hist(samples, pt_jet_ranges, max_delta_zeta=None, delta_le
                 jet_constituents = get_jet_constituents(event=r.event, index=hadronic_jet_idx, max_numb_of_cons=50)
 
                 if len(jet_constituents) > 0:
-                    if count < 10:                      # TEST-Line
-                        print(hadronic_jet_idx)         # TEST-Line
-                    triplets = make_triplets(jet_pt=hadronic_jet_pt, particle_vectors=jet_constituents,
-                                             top_data=bool(max_delta_zeta), w_data=(bool(delta_legs) and bool(shortest_side)),
-                                             pt_value=False)
-
                     if np.isnan(max_delta_zeta):
                         max_delta_zeta_calc = 3.5 / 3. * (170. / hadronic_jet_pt) ** 2
                     else:
                         max_delta_zeta_calc = max_delta_zeta
 
-                    if max_delta_zeta is not None:
-                        triplets_cut = (triplets[:, 2] < max_delta_zeta_calc)
-                        if delta_legs is not None and shortest_side is not None:
-                            triplets_cut = triplets_cut & (triplets[:, 3] < delta_legs) & (triplets[:, 4] < shortest_side)
-                    elif delta_legs is not None and shortest_side is not None:
-                        triplets_cut = (triplets[:, 2] < delta_legs) & (triplets[:, 3] < shortest_side)
-                    else:
-                        triplets_cut = np.full(triplets.shape[0], True, dtype=bool)
+                    triplets = make_triplets_and_cut(jet_pt=hadronic_jet_pt, particle_vectors=jet_constituents,
+                                                     max_delta_zeta=max_delta_zeta_calc,
+                                                     delta_legs=delta_legs, shortest_side=shortest_side)
 
                     k = None
                     for i, jet_range in enumerate(pt_jet_ranges):
                         if jet_range[0] <= hadronic_jet_pt < jet_range[1]:
+                            # for three_zeta, weight in zip(triplets[0], triplets[1]):
+                            #     hists[h][k].Fill(three_zeta, weight)
                             k = i
                             break
 
                     if k is not None:
-                        np_hists[h][k] = np_hists[h][k] + np.histogram(triplets[triplets_cut, 0],
-                                                                       weights=triplets[triplets_cut, 1],
+                        np_hists[h][k] = np_hists[h][k] + np.histogram(triplets[0], weights=triplets[1],
                                                                        bins=nbins, range=hist_range, density=False)[0]
                         # ToDo: Test if it is faster to directly fill the root histogram
                     count += 1
 
-    np_hist_bins = np.histogram(triplets[triplets_cut, 0], weights=triplets[triplets_cut, 1],
+    np_hist_bins = np.histogram(triplets[0], weights=triplets[1],
                                 bins=nbins, range=hist_range, density=False)[1]
 
     return np_hists, np_hist_bins
+
+
+def save_root_hists(root_hists, sample_names, pt_jet_ranges, filename):
+    # type: (list, list, list, str) -> None
+
+    f = ROOT.TFile(filename, 'RECREATE')
+    f.cd()
+
+    for h, sample_name in enumerate(sample_names):
+        for k, pt_jet_range in enumerate(pt_jet_ranges):
+            root_hists[h][k].Write('correlator_hist_{:}_{:}_{:}'.format(sample_name, pt_jet_range[0], pt_jet_range[1]))
+
+    f.Close()
 
 
 if __name__ == '__main__':
@@ -87,8 +91,8 @@ if __name__ == '__main__':
     argParser.add_argument('--job', action='store', type=int, default=0)
     args = argParser.parse_args()
 
-    mc_ = [UL2018.TTbar_1]            # + [UL2018.TTbar_2, UL2018.TTbar_3]
-    samples = [sample.split(n=args.nJobs, nSub=args.job) for sample in mc_]
+    mc = [UL2018.TTbar_1, UL2018.TTbar_2, UL2018.TTbar_3]
+    samples = [sample.split(n=args.nJobs, nSub=args.job) for sample in mc]
 
     pt_jet_lowest = 400
     pt_jet_highest = 700
@@ -98,12 +102,14 @@ if __name__ == '__main__':
 
     start = time.time()
     count = 0
-    np_hists, np_hist_bins = calc_triplets_and_hist(samples=samples, pt_jet_ranges=pt_jet_ranges,
-                                                    max_delta_zeta=np.nan)
-    store_np_hist_in_root(numpy_hist=(np_hists, np_hist_bins), sample_names=[sample.name[:11] for sample in samples],
+    numpy_hist = calc_triplets_and_hist(samples=samples, pt_jet_ranges=pt_jet_ranges, max_delta_zeta=np.nan)
+    # save_root_hists(root_hists=hists, sample_names=[sample.name[:11] for sample in samples],
+    #                 pt_jet_ranges=pt_jet_ranges,
+    #                 filename='histogram_files/correlator_hist_trip_test_new_{:02}.root'.format(args.job))
+    store_np_hist_in_root(numpy_hist=numpy_hist, sample_names=[sample.name[:11] for sample in samples],
                           pt_jet_ranges=pt_jet_ranges,
                           filename='histogram_files/correlator_hist_trip_{:02}.root'.format(args.job))
     end = time.time()
 
     print('Executing calc_triplet_and_hist.py took {:.0f}:{:.2f} min:sec.'.format((end-start)//60, (end-start)%60))
-    print('Number of considered events: {:}'.format(count))
+    print('Number of considered events in all samples: {:}'.format(count))