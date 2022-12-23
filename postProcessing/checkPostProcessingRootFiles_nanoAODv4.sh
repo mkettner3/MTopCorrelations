3f7199762104ba7b3d119b4a166bc5fa9c61dab1
Matthias Kettner <matthias.kettner@student.tuwien.ac.at>
2022-12-23 13:24:10 +0100

    Deleted folder postProcessing as discussed with Dennis

--- postProcessing/checkPostProcessingRootFiles_nanoAODv4.sh
+++
@@ -1,8 +0,0 @@
-python checkPostProcessingRootFiles.py $@ --file nanoPostProcessing_Run2016_nanoAODv4  --createExec --data_directory /mnt/hephy/cms/robert.schoefbeck/tWZ/nanoTuples/
-python checkPostProcessingRootFiles.py $@ --file nanoPostProcessing_Summer16_nanoAODv4 --createExec --data_directory /mnt/hephy/cms/robert.schoefbeck/tWZ/nanoTuples/
-
-python checkPostProcessingRootFiles.py $@ --file nanoPostProcessing_Run2017_nanoAODv4  --createExec --data_directory /mnt/hephy/cms/robert.schoefbeck/tWZ/nanoTuples/
-python checkPostProcessingRootFiles.py $@ --file nanoPostProcessing_Fall17_nanoAODv4   --createExec --data_directory /mnt/hephy/cms/robert.schoefbeck/tWZ/nanoTuples/
-
-python checkPostProcessingRootFiles.py $@ --file nanoPostProcessing_Run2018_nanoAODv4  --createExec --data_directory /mnt/hephy/cms/robert.schoefbeck/tWZ/nanoTuples/
-python checkPostProcessingRootFiles.py $@ --file nanoPostProcessing_Autumn18_nanoAODv4 --createExec --data_directory /mnt/hephy/cms/robert.schoefbeck/tWZ/nanoTuples/
