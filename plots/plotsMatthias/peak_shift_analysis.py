47a8145e1393788387817394b06fc2a6f924ddef
Matthias Kettner <matthias.kettner@student.tuwien.ac.at>
2023-11-15 01:12:46 +0100

    Included Crystal-Ball-Fits for the jet-pT-variations and minor refactoring; However, there is still a bug in line 110

--- plots/plotsMatthias/peak_shift_analysis.py
+++ plots/plotsMatthias/peak_shift_analysis.py
@@ -52,7 +52,7 @@ def perform_gauss_fit(root_hist, window):
     sigma_gauss = gauss_fit.GetParameter(2)
     max_gauss = gauss_fit.GetMaximumX()
 
-    return gauss_fit, norm_gauss, mean_gauss, sigma_gauss, max_gauss
+    return norm_gauss, mean_gauss, sigma_gauss, max_gauss
 
 
 def perform_general_crystal_ball_fit(root_hist, window, norm_gauss, mean_gauss, sigma_gauss, double_sided=False):
@@ -90,19 +90,19 @@ def perform_general_crystal_ball_fit(root_hist, window, norm_gauss, mean_gauss, 
     crystal_ball_mean = CB_fit.GetParameter(1)
     crystal_ball_mean_error = CB_fit.GetParError(1)
     crystal_ball_max = CB_fit.GetMaximumX()
-    standard_parameters = [CB_fit.GetParameter(i) for i in range(2, CBFunction.GetNpar())]
+    standard_parameters = [CB_fit.GetParameter(i) for i in range(2, CB_fit.GetNpar())]
 
     print(CB_fit.GetParameter(3), CB_fit.GetParameter(4))
 
-    return CB_fit, crystal_ball_mean, crystal_ball_mean_error, crystal_ball_max, standard_parameters
+    return crystal_ball_mean, crystal_ball_mean_error, crystal_ball_max, standard_parameters
 
 
 def perform_standardized_crystal_ball_fit(root_hist, window, norm_gauss, mean_gauss, standard_params, double_sided=False):
     def function_CB(x, params):
         if double_sided:
-            DoubleSidedCB(x, list(params)+list(standard_params))
+            DoubleSidedCB(x, list(params)+standard_params)
         else:
-            OneSidedCB(x, list(params)+list(standard_params))
+            OneSidedCB(x, list(params)+standard_params)
 
     CBFunction = ROOT.TF1("CBFunction", function_CB, window[0], window[1], 2)
     CBFunction.SetParameters(norm_gauss, mean_gauss)
@@ -116,7 +116,7 @@ def perform_standardized_crystal_ball_fit(root_hist, window, norm_gauss, mean_ga
     ndf = CB_fit.GetNDF()
     chi2 = CB_fit.GetChisquare()
 
-    return CB_fit, crystal_ball_mean, crystal_ball_mean_error, crystal_ball_max, chi2, chi2/ndf
+    return crystal_ball_mean, crystal_ball_mean_error, crystal_ball_max, chi2, chi2/ndf
 
 
 def DoubleSidedCB(x, params):
@@ -254,8 +254,8 @@ def draw_mass_fit_graph(correlator_values_variations, filename_graphic, chart_ti
     c.Print(plot_directory+filename_graphic)
 
 
-def generate_fits(rew_samples, mtop_bw_names, filename, subfolder, pt_jet_range, peak_window, variation_id=''):
-    # type: (list, list, str, str, tuple, tuple, str) -> tuple
+def generate_fits(rew_samples, mtop_bw_names, filename, subfolder, pt_jet_range, peak_window, variation_id='', bin_number=30):
+    # type: (list, list, str, str, tuple, tuple, str, int) -> tuple
 
     gauss_means = {}
     gauss_maximums = {}
@@ -263,46 +263,45 @@ def generate_fits(rew_samples, mtop_bw_names, filename, subfolder, pt_jet_range,
     CB_mean_errors = {}
     CB_maximums = {}
 
-    # hist = prepare_histogram(filename_root_hist=filename,
-    #                          hist_name='/Top-Quark/Gen-Level/weighted/correlator_hist_Gen_{:}_{:}_{:}'.format(173.0, pt_jet_range[0], pt_jet_range[1]),
-    #                          bin_number=30)
-    # peak_max_bin, peak_max_value = find_peak_argmax(root_hist=hist, window=peak_window)
-    # _, gauss_norm, gauss_mean, gauss_sigma, _ = perform_gauss_fit(root_hist=hist, window=peak_window)
-    # (CB_fit_func, CB_mean, CB_mean_error,
-    #  CB_max, standard_param) = perform_general_crystal_ball_fit(root_hist=hist, window=peak_window, norm_gauss=gauss_norm,
-    #                                                             mean_gauss=gauss_mean, sigma_gauss=gauss_sigma, double_sided=False)
+    hist = prepare_histogram(filename_root_hist=filename,
+                             hist_name='/Top-Quark/Gen-Level/weighted/correlator_hist_Gen_{:}_{:}_{:}'.format(173.0, pt_jet_range[0], pt_jet_range[1]),
+                             bin_number=bin_number)
+    gauss_norm, gauss_mean, gauss_sigma, _ = perform_gauss_fit(root_hist=hist, window=peak_window)
+    _, _, _, standard_param = perform_general_crystal_ball_fit(root_hist=hist, window=peak_window, norm_gauss=gauss_norm,
+                                                               mean_gauss=gauss_mean, sigma_gauss=gauss_sigma, double_sided=False)
 
     for mtop_bw, mtop_bw_name in zip(rew_samples, mtop_bw_names):
         hist = prepare_histogram(filename_root_hist=filename,
                                  hist_name='/Top-Quark/Gen-Level/weighted/correlator_hist{:}_Gen_{:}_{:}_{:}'.format(variation_id, mtop_bw, pt_jet_range[0], pt_jet_range[1]),
-                                 bin_number=30)
+                                 bin_number=bin_number)
 
         # Find the maximum bin
         peak_max_bin, peak_max_value = find_peak_argmax(root_hist=hist, window=peak_window)
         print('Maximum bin number: {:}; Maximum value: {:}'.format(peak_max_bin, peak_max_value))
 
         # Perform a Gauss fit to get a rough idea of fit parameters
-        gauss_fit_func, gauss_norm, gauss_mean, gauss_sigma, gauss_max = perform_gauss_fit(root_hist=hist, window=peak_window)
+        gauss_norm, gauss_mean, gauss_sigma, gauss_max = perform_gauss_fit(root_hist=hist, window=peak_window)
         print('Gauss-Fit mean: {:.3f}; Gauss-Fit Sigma: {:.3f}'.format(gauss_mean, gauss_sigma))
         draw_histogram(root_hist=hist, filename_graphic=subfolder+'/peak_fitting/correlator_gauss_fit{:}_Gen_{:}_{:}-{:}.pdf'.format(variation_id, mtop_bw_name, pt_jet_range[0], pt_jet_range[1]),
                        sample_name=mtop_bw_name, fit_label='Gauss-Fit', fit_function='gaussFitFunction')
 
         # Now fit with double-sided crystal ball function
-        # (CB_fit_func, CB_mean, CB_mean_error,
-        #  CB_max, CB_chi2, CB_chi2_ndf) = perform_standardized_crystal_ball_fit(root_hist=hist, window=peak_window, norm_gauss=gauss_norm,
-        #                                                                        mean_gauss=gauss_mean, standard_params=standard_param, double_sided=False)
-        # print('Crystal-Ball-Fit mean: {:.3f} +- {:.3f}; Chi2: {:.3f}; Chi2/NDF: {:.3f}'.format(CB_mean, CB_mean_error, CB_chi2, CB_chi2_ndf))
-        # draw_histogram(root_hist=hist, filename_graphic=subfolder+'/peak_fitting/correlator_CB_fit_Gen_{:}_{:}-{:}.pdf'.format(mtop_bw_name, pt_jet_range[0], pt_jet_range[1]),
-        #                sample_name=mtop_bw_name, fit_label='Crystal-Ball-Fit', fit_function='CBFunction')
+        (CB_mean, CB_mean_error, CB_max,
+         CB_chi2, CB_chi2_ndf) = perform_standardized_crystal_ball_fit(root_hist=hist, window=peak_window, norm_gauss=gauss_norm,
+                                                                       mean_gauss=gauss_mean, standard_params=standard_param, double_sided=False)
+        print('Crystal-Ball-Fit mean: {:.3f} +- {:.3f}; Chi2: {:.3f}; Chi2/NDF: {:.3f}'.format(CB_mean, CB_mean_error, CB_chi2, CB_chi2_ndf))
+        draw_histogram(root_hist=hist, filename_graphic=subfolder+'/peak_fitting/correlator_CB_fit{:}_Gen_{:}_{:}-{:}.pdf'.format(variation_id, mtop_bw_name, pt_jet_range[0], pt_jet_range[1]),
+                       sample_name=mtop_bw_name, fit_label='Crystal-Ball-Fit', fit_function='CBFunction')
+
         gauss_means[mtop_bw if mtop_bw is not None else 172.5] = gauss_mean
         gauss_maximums[mtop_bw if mtop_bw is not None else 172.5] = gauss_max
-        # CB_means[mtop_bw if mtop_bw is not None else 172.5] = CB_mean
-        # CB_mean_errors[mtop_bw if mtop_bw is not None else 172.5] = CB_mean_error
-        # CB_maximums[mtop_bw if mtop_bw is not None else 172.5] = CB_max
+        CB_means[mtop_bw if mtop_bw is not None else 172.5] = CB_mean
+        CB_mean_errors[mtop_bw if mtop_bw is not None else 172.5] = CB_mean_error
+        CB_maximums[mtop_bw if mtop_bw is not None else 172.5] = CB_max
 
         print(' ')
 
-    return gauss_means, gauss_maximums
+    return gauss_means, gauss_maximums, CB_means, CB_mean_errors, CB_maximums
 
 
 if __name__ == '__main__':
@@ -323,18 +322,25 @@ if __name__ == '__main__':
 
     gauss_means_all_jet_pt_var = []
     gauss_maximums_all_jet_pt_var = []
+    CB_means_all_jet_pt_var = []
+    CB_maximums_all_jet_pt_var = []
     for var_id, var_name in zip(['']+jet_pt_var_ids, ['original']+jet_pt_var_names):
-        gauss_means, gauss_maximums = generate_fits(rew_samples=rew_samples, mtop_bw_names=mtop_bw_names, filename=filename, subfolder=subfolder,
-                                                    pt_jet_range=pt_jet_range, peak_window=peak_window, variation_id=var_id)
+        (gauss_means, gauss_maximums,
+         CB_means, CB_mean_errors, CB_maximums) = generate_fits(rew_samples=rew_samples, mtop_bw_names=mtop_bw_names, filename=filename, subfolder=subfolder,
+                                                                pt_jet_range=pt_jet_range, peak_window=peak_window, variation_id=var_id)
         gauss_means_all_jet_pt_var.append((var_name, gauss_means))
         gauss_maximums_all_jet_pt_var.append((var_name, gauss_maximums))
+        CB_means_all_jet_pt_var.append((var_name, CB_means))
+        CB_maximums_all_jet_pt_var.append((var_name, CB_maximums))
 
     draw_mass_fit_graph(correlator_values_variations=gauss_means_all_jet_pt_var,
                         filename_graphic=subfolder+'/peak_fitting/top_mass_gauss_fit_jet.pdf', chart_title='Top Mass Gauss Fit (Means)')
     draw_mass_fit_graph(correlator_values_variations=gauss_maximums_all_jet_pt_var,
                         filename_graphic=subfolder+'/peak_fitting/top_mass_gauss_max_fit_jet.pdf', chart_title='Top Mass Gauss Fit (Maximums)')
-    # draw_mass_fit_graph(correlator_values=CB_means, filename_graphic=subfolder+'/peak_fitting/top_mass_CB_fit.pdf', chart_title='Top Mass Crystal Ball Fit (Means)') # , correlator_value_errors=CB_mean_errors
-    # draw_mass_fit_graph(correlator_values=CB_maximums, filename_graphic=subfolder+'/peak_fitting/top_mass_CB_max_fit.pdf', chart_title='Top Mass Crystal Ball Fit (Maximums)')
+    draw_mass_fit_graph(correlator_values_variations=CB_means_all_jet_pt_var,
+                        filename_graphic=subfolder+'/peak_fitting/top_mass_CB_fit_jet.pdf', chart_title='Top Mass Crystal Ball Fit (Means)') # , correlator_value_errors=CB_mean_errors
+    draw_mass_fit_graph(correlator_values_variations=CB_maximums_all_jet_pt_var,
+                        filename_graphic=subfolder+'/peak_fitting/top_mass_CB_max_fit_jet.pdf', chart_title='Top Mass Crystal Ball Fit (Maximums)')
 
     gauss_means_all_cons_pt_var = []
     gauss_maximums_all_cons_pt_var = []
