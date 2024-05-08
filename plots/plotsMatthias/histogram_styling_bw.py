8ed04776bb59a33d404e220996b03576fda23592
Matthias Kettner <matthias.kettner@student.tuwien.ac.at>
2024-05-08 12:12:30 +0200

    Improved histogram styling for thesis

--- plots/plotsMatthias/histogram_styling_bw.py
+++ plots/plotsMatthias/histogram_styling_bw.py
@@ -1,9 +1,9 @@
 import ROOT
 from MTopCorrelations.Tools.user import plot_directory
-from calc_triplet_and_hist import pt_jet_ranges
+from calc_triplet_and_hist_bw import pt_jet_ranges
 
 
-def style_corr_hist(filename_root, hist_name, sample_names, filename_graphic, ylim=(0, 0.0005), verb=True):
+def style_corr_hist(filename_root, hist_name, sample_names, filename_graphic, ylim=(0, 0.0005), all_ranges=False, verb=True):
     ROOT.gStyle.SetLegendBorderSize(0)  # No border for legend
     ROOT.gStyle.SetPadTickX(1)          # Axis ticks on top
     ROOT.gStyle.SetPadTickY(1)          # Axis ticks right
@@ -15,7 +15,10 @@ def style_corr_hist(filename_root, hist_name, sample_names, filename_graphic, yl
         hists[i].SetDirectory(ROOT.nullptr)
     f.Close()
     c = ROOT.TCanvas('c', 'c', 600, 600)
-    legend = ROOT.TLegend(0.65, 0.5, 0.85, 0.87)
+    if all_ranges:
+        legend = ROOT.TLegend(0.63, 0.55, 0.87, 0.87)
+    else:
+        legend = ROOT.TLegend(0.65, 0.5, 0.85, 0.87)
     ROOT.gPad.SetLeftMargin(0.12)
     ROOT.gPad.SetBottomMargin(0.12)
 
@@ -34,7 +37,10 @@ def style_corr_hist(filename_root, hist_name, sample_names, filename_graphic, yl
         hist.SetTitle('')
         hist.SetLineWidth(2)
         hist.SetLineStyle(1)
-        legend.AddEntry(hist, 'm_{top} = '+sample_name+' GeV', 'l')
+        if all_ranges:
+            legend.AddEntry(hist, 'p_{T,jet} #in ['+sample_name.replace('_','-')+'[ GeV', 'l')
+        else:
+            legend.AddEntry(hist, 'm_{top} = '+sample_name+' GeV', 'l')
         if hist_name[-6:] != 'abscou':
             hist.Scale(1/hist.Integral())
     hists[0].GetXaxis().SetRangeUser(0, 2.5)    # x-axis range (also works for y-axis)
@@ -43,7 +49,7 @@ def style_corr_hist(filename_root, hist_name, sample_names, filename_graphic, yl
     hists[0].GetXaxis().SetNdivisions(505)      # Unterteilung der x-Achse
     hists[0].GetXaxis().SetTitleOffset(1.5)
     hists[0].GetYaxis().SetRangeUser(ylim[0], ylim[1])
-    hists[0].GetYaxis().SetTitle("Energy-weighted Triplets")
+    # hists[0].GetYaxis().SetTitle("Energy-weighted Triplets")
     hists[0].GetYaxis().CenterTitle(ROOT.kTRUE)
     hists[0].GetYaxis().SetNdivisions(505)      # Unterteilung der y-Achse
     hists[0].GetYaxis().SetMaxDigits(3)     # 3 ist die einzig sinnvolle Einstellung, weil als Exponent der Zehnerpotenz nur Vielfache von 3 verwendet werden.
@@ -135,7 +141,7 @@ def style_jet_hist(filename_root, sample_names, hist_name, filename_graphic, xli
         hists[i].SetDirectory(ROOT.nullptr)
     f.Close()
     c = ROOT.TCanvas('c', 'c', 600, 600)
-    legend = ROOT.TLegend(0.65, 0.5, 0.85, 0.87)
+    legend = ROOT.TLegend(0.48, 0.30, 0.85, 0.87)
     ROOT.gPad.SetLeftMargin(0.12)
     ROOT.gPad.SetBottomMargin(0.12)
 
@@ -164,12 +170,12 @@ def style_jet_hist(filename_root, sample_names, hist_name, filename_graphic, xli
     hists[0].GetXaxis().SetRangeUser(xlim[0], xlim[1])
     hists[0].GetXaxis().SetTitle(title)
     hists[0].GetXaxis().CenterTitle(ROOT.kTRUE)
-    hists[0].GetXaxis().SetNdivisions(505)      # Unterteilung der x-Achse
+    hists[0].GetXaxis().SetNdivisions(5, 5, 0)      # Unterteilung der x-Achse
     hists[0].GetXaxis().SetTitleOffset(1.5)
     hists[0].GetYaxis().SetRangeUser(ylim[0], ylim[1])
-    hists[0].GetYaxis().SetTitle("Number of Events")
+    hists[0].GetYaxis().SetTitle("number of events")
     hists[0].GetYaxis().CenterTitle(ROOT.kTRUE)
-    hists[0].GetYaxis().SetNdivisions(505)      # Unterteilung der y-Achse
+    hists[0].GetYaxis().SetNdivisions(5, 5, 0)      # Unterteilung der y-Achse
     hists[0].GetYaxis().SetMaxDigits(3)     # 3 ist die einzig sinnvolle Einstellung, weil als Exponent der Zehnerpotenz nur Vielfache von 3 verwendet werden.
     hists[0].GetYaxis().SetTitleOffset(1.5)
 
@@ -189,6 +195,11 @@ if __name__ == '__main__':
     ROOT.gROOT.SetBatch(ROOT.kTRUE)             # Prevent graphical display for every c.Print() statement
 
     for level in ['Gen', 'PF']:
+        style_corr_hist(filename_root=filename,
+                        hist_name='/Top-Quark/'+level+'-Level/weighted/correlator_hist_{:}_None_$'.format(level),
+                        sample_names=[str(elem[0])+'_'+str(elem[1]) for elem in pt_jet_ranges],
+                        filename_graphic=subfolder+'/correlator_hist_{:}_172p5_all_pt_ranges.pdf'.format(level),
+                        ylim=(0, 0.007), all_ranges=True, verb=False)
         for pt_range in pt_jet_ranges:
             style_corr_hist(filename_root=filename,
                             hist_name='/Top-Quark/'+level+'-Level/weighted/correlator_hist_{:}_$_{:}_{:}'.format(level, pt_range[0], pt_range[1]),
@@ -214,6 +225,7 @@ if __name__ == '__main__':
                             filename_graphic=subfolder+'/correlator_hist_W_{:}_{:}-{:}_abs.pdf'.format(level, pt_range[0], pt_range[1]),
                             ylim=(0, 400000), verb=False)
 
+            """
             style_varied_hist(filename_root=filename,
                               hist_name='/Top-Quark/'+level+'-Level/weighted/correlator_hist_{:}_None_{:}_{:}'.format(level, pt_range[0], pt_range[1]),
                               varied_hist_name='/Top-Quark/'+level+'-Level/weighted/correlator_hist_varied_jet_$_{:}_None_{:}_{:}'.format(level, pt_range[0], pt_range[1]),
@@ -234,24 +246,25 @@ if __name__ == '__main__':
             #                   var_factors=['1.02', '0.98'],
             #                   filename_graphic=subfolder+'/correlator_hist_varied_cons_eta_phi_{:}_{:}-{:}.png'.format(level, pt_range[0], pt_range[1]),
             #                   ylim=(0, 0.02), verb=False)
+            """
 
         style_jet_hist(filename_root=filename,
                        hist_name='/Others/'+level+'-Level/hadronic_top_jet_pt_hist_{:}_$'.format(level),
                        sample_names=sample_names,
                        filename_graphic=subfolder+'/hadronic_top_jet_pt_hist_{:}.pdf'.format(level),
-                       xlim=(380, 730), ylim=(0, 2000), rebin=20, title='Hadronic Top-Jet-p_{T} [GeV]')
+                       xlim=(380, 730), ylim=(0, 2000), rebin=20, title='jet p_{T} [GeV]')
 
         style_jet_hist(filename_root=filename,
                        hist_name='/Others/'+level+'-Level/hadronic_top_constituents_pt_hist_{:}_$'.format(level),
                        sample_names=sample_names,
                        filename_graphic=subfolder+'/hadronic_top_jet_constituents_pt_hist_{:}.pdf'.format(level),
-                       xlim=(0, 30), ylim=(0, 250000), rebin=20, title='Hadronic Top-Jet-Constituents-p_{T} [GeV]')
+                       xlim=(0, 30), ylim=(0, 250000), rebin=20, title='jet constituents p_{T} [GeV]')
 
         style_jet_hist(filename_root=filename,
                        hist_name='/Others/'+level+'-Level/hadronic_top_jet_mass_hist_{:}_$'.format(level),
                        sample_names=sample_names,
                        filename_graphic=subfolder+'/hadronic_top_jet_mass_hist_{:}.pdf'.format(level),
-                       xlim=(165, 190), ylim=(0, 2200), title='Hadronic Top-Jet-Mass [GeV]')
+                       xlim=(165, 190), ylim=(0, 2200), title='jet mass [GeV]')
 
         style_jet_hist(filename_root=filename,
                        hist_name='/Others/'+level+'-Level/hadronic_top_jet_mass_hist_{:}_MC_$'.format(level),
